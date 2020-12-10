76bf496f871744480d9bd2d8015aadb20017580b
suzukey <suzukei0208@gmail.com>
2020-12-10 23:13:38 +0900

    🎨 共通部分の調整

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
@@ -8,7 +7,6 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/">
-            <img src={logo} className="App-logo" alt="logo" />
             <p>
               Edit <code>src/App.js</code> and save to reload.
             </p>
